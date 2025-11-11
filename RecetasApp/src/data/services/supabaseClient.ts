c85028029180c64770722717d89d4dd92e116243
CristianTambaco <1234cristiant@gmail.com>
2025-11-10 22:28:58 -0500

    modified:   RecetasApp/package-lock.json 	modified:   RecetasApp/package.json 	modified:   RecetasApp/src/data/services/supabaseClient.ts

--- RecetasApp/src/data/services/supabaseClient.ts
+++ RecetasApp/src/data/services/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
+import AsyncStorage from "@react-native-async-storage/async-storage"; //
+
 // Obtenemos las credenciales desde las variables de entorno
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
@@ -13,11 +15,25 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   );
 }
 
+// Definimos el objeto de almacenamiento personalizado
+const supabaseStorage = {
+  getItem: (key: string) => {
+    return AsyncStorage.getItem(key);
+  },
+  setItem: (key: string, value: string) => {
+    return AsyncStorage.setItem(key, value);
+  },
+  removeItem: (key: string) => {
+    return AsyncStorage.removeItem(key);
+  },
+};
+
+
 // Creamos el cliente de Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     // No usamos AsyncStorage 
-    storage: undefined,
+    storage: supabaseStorage,
     // Refrescar token automáticamente cuando expire
     autoRefreshToken: true,
     // NO persistir sesión (se pierde al cerrar app)
